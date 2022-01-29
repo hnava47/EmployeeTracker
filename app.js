13fcfcd713797de08743921b4dd7e5217872145d
Hector Nava <hector.r.nava44@gmail.com>
2022-01-29 16:43:22 -0700

    Add employee function added

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ const getRoles = async() => {
   return totalRoles;
 };
 
+const getManagers = async() => {
+  const getManagerQuery = 'SELECT CONCAT(first_name, " ", last_name) name FROM employee;';
+  const [managers] = await connection.query(getManagerQuery);
+
+  let totalManagers = ['None'];
+  for (let i=0; i<managers.length; i++) {
+    totalManagers.push(managers[i].name.toString());
+  }
+
+  return totalManagers;
+}
+
 const viewEmp = async() => {
   try {
     const getEmpQuery = 'SELECT * FROM employee;';
@@ -64,11 +76,37 @@ const getQuestion = () => {
               message: "What is the employee's role?",
               name: 'role',
               choices: getRoles
+            },
+            {
+              type: 'list',
+              message: "Who is the employee's manager?",
+              name: 'manager',
+              choices: getManagers
             }
           ]).then(async(response) => {
+            try {
+              const managerName = response.manager.split(' ');
+              const insertEmp = 'INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?, ?, ?, ?);';
+              const roleIdQuery = 'SELECT id FROM role WHERE title = ?;';
+              const managerIdQuery = 'SELECT id FROM employee WHERE first_name = ? AND last_name = ?;';
 
+              const [roleId] = await connection.query(roleIdQuery, response.role);
+              const [managerId] = await connection.query(managerIdQuery, [managerName[0], managerName[1]]);
+              await connection.query(insertEmp, [response.firstName, response.lastName, roleId[0].id, managerId[0].id]);
+
+              console.log(`Added ${response.firstName} ${response.lastName} to the database`);
+
+              getQuestion();
+            } catch (e) {
+              console.log(e);
+            }
           });
+      } else if (response.action === questions[2]) {
+
+      } else if (response.action === questions[3]) {
+        
       }
+
     });
 };
 
