81b8f9c1f4d55e50c2e99019e85e42a18511f1bf
Hector Nava <hector.r.nava44@gmail.com>
2022-01-29 11:37:17 -0700

    Add get roles query to populate add employee question

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const inquirer = require('inquirer');
 const connection = require('./config');
 require('console.table');
 
+const questions = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+
+const getRoles = async() => {
+  const getRolesQuery = 'SELECT title FROM role;';
+  const [roles] = await connection.query(getRolesQuery);
+
+  let totalRoles = [];
+  for (let i=0; i<roles.length; i++) {
+    totalRoles.push(roles[i].title.toString());
+  }
+
+  return totalRoles;
+};
+
 const viewEmp = async() => {
   try {
     const getEmpQuery = 'SELECT * FROM employee;';
@@ -19,19 +33,41 @@ const getQuestion = () => {
         type: 'list',
         message: 'What would you like to do?',
         name: 'action',
-        choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        choices: questions
       }
     ]).then(async(response) => {
       if (response.action === 'Quit') {
         console.log('Exiting program');
         process.exit();
-      } else if (response.action === 'View All Employees') {
+      } else if (response.action === questions[0]) {
         try {
           await viewEmp();
           await getQuestion();
         } catch (error) {
           console.log(e);
         }
+      } else if (response.action === questions[1]) {
+        inquirer
+          .prompt([
+            {
+              type: 'input',
+              message: "What is the employee's first name?",
+              name: 'firstName'
+            },
+            {
+              type: 'input',
+              message: "What is the employee's last name?",
+              name: 'lastName'
+            },
+            {
+              type: 'list',
+              message: "What is the employee's role?",
+              name: 'role',
+              choices: getRoles
+            }
+          ]).then(async(response) => {
+
+          });
       }
     });
 };
