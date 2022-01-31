37e7720b7a41aade7787a946a31df8d021f44e90
Hector Nava <hector.r.nava44@gmail.com>
2022-01-31 13:36:46 -0700

    Added dotenv package

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config()
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'employee_db'
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: process.env.DB_NAME
 }).promise();
 
 module.exports = connection;
