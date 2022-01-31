c74b62747e4b0bebc38a47d72f79b6128bccc5ab
Hector Nava <hector.r.nava44@gmail.com>
2022-01-31 16:14:03 -0700

    Updated salary data type to decimal with 2 decimals

--- db/schema.sql
+++ db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE department(
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL DEFAULT 0,
+    salary DECIMAL(19,2) NOT NULL DEFAULT 0,
     department_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
